622b900b50c27000a771fd2b7af6c2dbeef2b02d
monkbangg <monkbangg@gmail.com>
2023-02-16 17:31:39 +0900

    feat: compile to es6

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -32,15 +41,15 @@ const nock_1 = __importDefault(require("nock"));
 const index_1 = __importDefault(require("../index"));
 beforeEach(() => {
     jest.resetModules();
-    process.env.GITHUB_TOKEN = "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";
+    process.env.GITHUB_TOKEN = "";
     github.context.payload = {
         repository: {
             owner: {
-                login: "AlmSmartDoctor",
+                login: "",
             },
-            name: "git-trunk-project",
+            name: "",
         },
-        number: 48,
+        number: 1,
         commits: [],
     };
 });
@@ -50,7 +59,7 @@ afterAll(() => {
     nock_1.default.cleanAll();
 });
 describe("debug action debug messages", () => {
-    it("isPullRequest is true", async () => {
+    it("isPullRequest is true", () => __awaiter(void 0, void 0, void 0, function* () {
         const tokenNumber = jest
             .spyOn(core, "getInput")
             .mockImplementation((name) => {
@@ -58,8 +67,8 @@ describe("debug action debug messages", () => {
                 return "true";
             return "";
         });
-        await (0, index_1.default)();
+        yield (0, index_1.default)();
         expect(tokenNumber.mock.results.length === 4);
         expect(tokenNumber.mock.results[0].value).toMatch("true");
-    });
+    }));
 });
--- index.js
+++ index.js
@@ -22,53 +22,69 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
 const rest_1 = require("@octokit/rest");
 const _ = __importStar(require("lodash"));
-async function getAllCommits(octokit, { owner, repo, pr }, page) {
-    const list = await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}/commits", {
-        owner,
-        repo,
-        pull_number: pr,
-        page,
-        per_page: 100,
-    });
-    if (list.data.length === 0)
-        return [];
-    return [
-        ...list.data,
-        ...(await getAllCommits(octokit, { owner, repo, pr }, page + 1)),
-    ];
-}
-async function extractJiraKeysFromCommit() {
-    try {
-        const regex = /((([A-Za-z]+)|([0-9]+))+-\d+)/g;
-        const isPullRequest = core.getInput("is-pull-request") === "true";
-        const { payload } = github.context;
-        const token = process.env.GITHUB_TOKEN;
-        const octokit = new rest_1.Octokit({
-            auth: token,
+function getAllCommits(octokit, { owner, repo, pr }, page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const list = yield octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}/commits", {
+            owner,
+            repo,
+            pull_number: pr,
+            page,
+            per_page: 100,
         });
-        if (isPullRequest) {
-            const owner = payload?.repository?.owner.login || "";
-            const repo = payload?.repository?.name || "";
-            const prNum = payload.number;
-            const commits = await getAllCommits(octokit, { owner, repo, pr: prNum }, 1);
-            const result = _.uniq(commits
-                .map(({ commit }) => {
-                return (commit.message.match(regex) || []).filter((match) => match);
-            })
-                .flat()).join(",");
-            core.setOutput("jira-keys", result);
-        }
-    }
-    catch (error) {
-        core.setFailed(error);
-    }
+        if (list.data.length === 0)
+            return [];
+        return [
+            ...list.data,
+            ...(yield getAllCommits(octokit, { owner, repo, pr }, page + 1)),
+        ];
+    });
 }
-(async function run() {
-    await extractJiraKeysFromCommit();
+function extractJiraKeysFromCommit() {
+    var _a, _b;
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const regex = /((([A-Za-z]+)|([0-9]+))+-\d+)/g;
+            const isPullRequest = core.getInput("is-pull-request") === "true";
+            const { payload } = github.context;
+            const token = process.env.GITHUB_TOKEN;
+            const octokit = new rest_1.Octokit({
+                auth: token,
+            });
+            if (isPullRequest) {
+                const owner = ((_a = payload === null || payload === void 0 ? void 0 : payload.repository) === null || _a === void 0 ? void 0 : _a.owner.login) || "";
+                const repo = ((_b = payload === null || payload === void 0 ? void 0 : payload.repository) === null || _b === void 0 ? void 0 : _b.name) || "";
+                const prNum = payload.number;
+                const commits = yield getAllCommits(octokit, { owner, repo, pr: prNum }, 1);
+                const result = _.uniq(commits
+                    .map(({ commit }) => {
+                    return (commit.message.match(regex) || []).filter((match) => match);
+                })
+                    .flat()).join(",");
+                core.setOutput("jira-keys", result);
+            }
+        }
+        catch (error) {
+            core.setFailed(error);
+        }
+    });
+}
+(function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield extractJiraKeysFromCommit();
+    });
 })();
 exports.default = extractJiraKeysFromCommit;
